7e1b0ab4025d805d46fdfecf6185e7f6cfe9f32c
Gemma González Gil <ggg-95@hotmail.com>
2018-02-21 22:14:46 +0100

    Cambios

---
+++ RedSocialSDI/src/main/java/com/uniovi/entities/Publication.java
@@ -0,0 +1,112 @@
+package com.uniovi.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Publication {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String texto;
+
+	@ManyToOne
+	private User user;
+
+	@Column(name = "fecha_creacion")
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacion;
+
+	protected Publication() {
+	}
+
+	public Publication(String title, String texto, User user) {
+		super();
+		this.title = title;
+		this.texto = texto;
+		this.user = user;
+
+		Association.Publish.link(user, this);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	protected void _setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void deletePublication() {
+		Association.Publish.unlink(user, this);
+	}
+
+	@Override
+	public String toString() {
+		return "Publication [id=" + id + ", title=" + title + ", texto=" + texto + ", user=" + user + ", fechaCreacion="
+				+ fechaCreacion + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publication other = (Publication) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
--- src/main/java/com/uniovi/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniovi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.uniovi.entities.User;
-
-public interface UsersRepository extends CrudRepository<User, Long> {
-	User findByEmail(String email);
-}
