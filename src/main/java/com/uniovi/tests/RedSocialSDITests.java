28b7f11305b7851365bec918be2ce2cc6133877d
Gemma González Gil <ggg-95@hotmail.com>
2018-03-18 23:30:43 +0100

    Documentación publicación con foto y test a medias

--- src/main/java/com/uniovi/tests/RedSocialSDITests.java
+++ src/main/java/com/uniovi/tests/RedSocialSDITests.java
@@ -23,19 +23,15 @@ import com.uniovi.tests.pageobjects.PO_RegisterView;
 import com.uniovi.tests.pageobjects.PO_View;
 import com.uniovi.tests.utils.SeleniumUtils;
 
-//Ordenamos las pruebas por el nombre del método 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RedSocialSDITests {
 
-	// En Windows (Debe ser la versión 46.0 y desactivar las actualizacioens
-	// automáticas)):
-	static String PathFirefox = "C:\\Firefox46.win\\FirefoxPortable.exe";
-	// Común a Windows y a MACOSX
+	// C:\\Firefox46.win\\FirefoxPortable.exe
+	static String PathFirefox = "C:\\Users\\gemma\\Desktop\\RedSocialSDI\\Firefox46.win\\FirefoxPortable.exe";
 	static WebDriver driver = getDriver(PathFirefox);
 	static String URL = "http://localhost:8090";
 
 	public static WebDriver getDriver(String PathFirefox) {
-		// Firefox (Versión 46.0) sin geckodriver para Selenium 2.x.
 		System.setProperty("webdriver.firefox.bin", PathFirefox);
 		WebDriver driver = new FirefoxDriver();
 		return driver;
@@ -338,6 +334,32 @@ public class RedSocialSDITests {
 	// 12.1 Crear una publicación con datos válidos y una foto adjunta.
 	@Test
 	public void PubFoto1Val() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "email1", "123456");
+
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "btnPost", PO_View.getTimeout());
+		elementos.get(0).click();
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "postDropdownMenuButton", PO_View.getTimeout());
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "addPost", PO_View.getTimeout());
+		elementos.get(0).click();
+
+		PO_View.checkElement(driver, "text", "SDI - Red social");
+		WebElement title = driver.findElement(By.name("title"));
+		title.click();
+		title.clear();
+		title.sendKeys("Primera publicación");
+		WebElement text = driver.findElement(By.name("text"));
+		text.click();
+		text.clear();
+		text.sendKeys("Esto es una publicación de prueba");
+
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, "class", "btn btn-outline-primary", PO_View.getTimeout());
+		elementos.get(0).click();
+		// TODO: mirar como subir la foto desde aquí
+		By boton = By.id("submit");
+		driver.findElement(boton).click();
+
+		PO_View.checkElement(driver, "text", "Publicaciones");
 	}
 
 	// 12.1 Crear una publicación con datos válidos y sin una foto
